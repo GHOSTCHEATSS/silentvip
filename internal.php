00550b921a08983ffdac228e9f50f16706fda5ed
GHOSTCHEATSS <hassanrafiq269@gmail.com>
2024-07-24 09:41:48 -0700

    Delete internal.php

--- internal.php
+++
@@ -1,1 +0,0 @@
-https://cdn.discordapp.com/attachments/1240342325436219412/1265343045151363264/internal_protected.dll?ex=66a12a0a&is=669fd88a&hm=2a9489692bc809b8fd90d48c60f6c71251d05002ed53aef2ecec45c872f449ef&
